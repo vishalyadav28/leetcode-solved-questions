98ae0e290a4499956983f169fad96dfb5eb558ac
vishal yadav <74091477+vishalyadav28@users.noreply.github.com>
2023-07-03 04:14:51 +0000

    Added : Python and Ruby folder and pushed TwoSum solution in python

---
+++ python/Two-Sum.py
@@ -0,0 +1,22 @@
+#Solution #1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            n=target-nums[i]
+            if n in result:
+                return[i,nums.index(n)]
+            result.append(nums[i])
+
+#Solution #2
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # hash={}
+        # for key,value in enumerate(nums):
+        #     n = target - value
+        #     if n not in hash:
+        #         hash[value] = key
+        #     else:
+        #         return [hash[n],key]
+
+        
