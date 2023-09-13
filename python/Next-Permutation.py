27ab7ac923be5c4c5dfd72a77163388f6819c68a
vishal yadav <74091477+vishalyadav28@users.noreply.github.com>
2023-09-13 14:48:23 +0000

    permutation solution

--- python/Next-Permutation.py
+++ python/Next-Permutation.py
@@ -87,3 +87,21 @@ class Solution:
         return(nums)
 
         
+
+
+
+# def generate_permutations(arr, start=0):
+#     result = []
+#     if start == len(arr):
+#         result.append(arr.copy())
+#     else:
+#         for i in range(start, len(arr)):
+#             arr[start], arr[i] = arr[i], arr[start]
+#             result.extend(generate_permutations(arr, start + 1))
+#             arr[start], arr[i] = arr[i], arr[start]  # backtrack
+#     return result
+
+# # Example usage:
+# elements = [1, 2, 3]
+# print(generate_permutations(elements))
+
