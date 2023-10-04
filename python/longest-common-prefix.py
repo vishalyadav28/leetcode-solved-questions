4d934da834c7be3a377b1618800d1de0eae4b92a
vishal yadav <74091477+vishalyadav28@users.noreply.github.com>
2023-10-04 07:07:14 +0000

    longestCommonPrefix

---
+++ python/longest-common-prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return '' 
+        if len(strs)==1:
+            return strs[0]
+        
+        pref = strs[0]
+        plen=len(pref)
+        for s in strs[1:]:
+            while pref!=s[0:plen]:
+                pref = pref[0:(plen-1)]
+                plen-=1
+                if plen == 0:
+                    return("")
+        return pref
