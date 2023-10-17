85caee69af7463ddc136bbba620bea12c21bf9b3
vishal yadav <74091477+vishalyadav28@users.noreply.github.com>
2023-10-17 06:37:57 +0000

    search-in-rotated-sorted-array

--- python/search-in-rotated-sorted-array.py
+++ python/search-in-rotated-sorted-array.py
@@ -1,6 +1,21 @@
 def search(nums, target) :
     # Write your code here.
     # pass
+    #O(N)
+    # def search(arr, target) :
+    # n = len(nums)
+    
+    # # Iterate through the array
+    # for i in range(n) :
+        
+    #     # target is found at ith index
+    #     if(nums[i] == target) :
+    #         return i
+
+    # # target not found
+    # return -1
+    
+    #O(logn)
     left, right = 0, len(nums) - 1
     
     while left <= right:
@@ -21,7 +36,4 @@ def search(nums, target) :
                 right = mid - 1
     
     return -1
-            
-
-
     
