12f4a1b3d599c6d14309a9e85610912828f54e67
vishal yadav <74091477+vishalyadav28@users.noreply.github.com>
2024-01-19 06:18:38 +0000

    learning recursion

---
+++ python/Print-1-to-N-using-recursion.py
@@ -0,0 +1,17 @@
+#normal logic (better to use this)
+
+# def print_number(i,n):
+#     print(i)
+#     if i!=n:
+#         print_number(i+1,n)
+# print_number(1,5)
+
+
+
+# #reverse the logic
+
+# def print_number(n):
+#     print(n)
+#     if n!=1:
+#         print_number(n-1)
+# print_number(5)
